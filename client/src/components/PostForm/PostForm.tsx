57c069fec9a417c59428ee77012911a6e6c79f87
Tatyana Rubtsova <liotic_ala@list.ru>
2024-11-11 17:43:29 +0500

    form validation with react-hook-form

--- client/src/components/PostForm/PostForm.tsx
+++ client/src/components/PostForm/PostForm.tsx
@@ -1,22 +1,36 @@
-import { FC, FormEventHandler, useState } from 'react';
+import { FC } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { z } from "zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button } from '../Button';
 import { FormField } from '../FormField';
 import './PostForm.css';
-import { useMutation } from '@tanstack/react-query';
 import { createPost } from '../../api/Post';
 import { queryClient } from '../../api/queryClient';
 
 export interface IPostFormProps {}
 
-export const PostForm: FC<IPostFormProps> = () => {
-  const [text, setText] = useState("");
+const CreatePostSchema = z.object({
+  text: z.string().min(10, "Длина поста должнабыть не менее 10 символов"),
+})
 
-  const [errorMessage, setErrorMessage] = useState<string | undefined>();
+type CreatePostForm = z.infer<typeof CreatePostSchema>;
+
+export const PostForm: FC<IPostFormProps> = () => {
+
+  const { 
+    register, 
+    handleSubmit,
+    formState: { errors},
+  } = useForm<CreatePostForm>({
+    resolver: zodResolver(CreatePostSchema),
+  })
 
   const createPostMutation = useMutation(
     {
-      mutationFn: () => createPost(text),
+      mutationFn: createPost,
       onSuccess() {
         queryClient.invalidateQueries({queryKey: ["posts"] });
       },
@@ -24,31 +38,15 @@ export const PostForm: FC<IPostFormProps> = () => {
     queryClient
   );
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-
-    if (text.length > 10) {
-      createPostMutation.mutate();
-    } else {
-      setErrorMessage("Введите более 10 символов");
-    }
-
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="post-form">
-      <FormField label="Текст поста">
-        <textarea 
-          className="post-form__input"
-          value={text}
-          onChange={(event) => {
-            setText(event.currentTarget.value);
-            setErrorMessage(undefined);
-          }}
-        />
+    <form className="post-form" 
+      onSubmit={handleSubmit(({ text }) => {
+        createPostMutation.mutate(text);
+      })}
+    >
+      <FormField label="Текст поста" errorMessage={errors.text?.message}>
+        <textarea className="post-form__input" {... register("text")}/>
       </FormField>
-
-      {errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
 
       <Button 
         type="submit" 
